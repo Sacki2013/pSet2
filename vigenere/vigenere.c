44779960e8e072b288d68e4677380f61803794e9
Adam Sackfield <Sacki2013@gmail.com>
2015-09-04 00:18:51 +0100

    Started over and nailing it now. Got the keyword convert to numbers and also got the message iterating

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -4,34 +4,39 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * keyword[])
+{
 
-  // Initialize variables and a boolean set to false to hold the user
-  // in the do loop untill they satisfy criteria.
+  int cipher[24], i, n;
   bool passed = false;
-  int length = 0, i, n, j, k;
-  char *message = malloc(256);
-  char *keyword = malloc(256);
-  int cipher[1000];
+  char * message = malloc(256);
 
   do {
     if (argc != 2) {
       printf("Error\n");
       return 1;
-    } else {
-        keyword = argv[1];
-        printf("%s\n", keyword);
-        passed = true;
+    }
+    else {
+      // cipher[i] is the ACII code of each number.
+      for (i = 0, n = strlen(keyword[1]); i < n; i++) {
+        cipher[i] = keyword[1][i];
+        printf("%i\n", cipher[i]);
+      }
+      passed = true;
     }
   } while(!passed);
 
-  // Prompt the user for something to encrypt. Then store in phrase.
-  printf("What would you like to encrypt?\n");
+  // Prompt the user for a message to encrypt
+  printf("Enter your secret message\n");
   fgets(message, 256, stdin);
 
-  for (i = 0, n = strlen(keyword); i < n; i++) {
-    char c = (((int)keyword[i] + 1));
-// Working so far it can get input move it and output new one.
-    printf("%c\n", (((int)c)));
+  // Iterate the message
+  for (i = 0, n = strlen(message); i < n - 1; i++) {
+    printf("%c\n", message[i] + cipher[i]);
+
   }
+
+
+
+
 }
